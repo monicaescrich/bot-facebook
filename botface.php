65722260f27de81ca45a089c1d5ad311b67c4e95
monicaescrich <monica.escrichc@gmail.com>
2016-05-18 15:36:32 -0600

    Update botface.php

--- botface.php
+++ botface.php
@@ -26,10 +26,11 @@ $response = [
     'message' => [ 'text' => $answer ]
 ];
 
-?>
+
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_exec($ch);
 curl_close($ch);
+?>
